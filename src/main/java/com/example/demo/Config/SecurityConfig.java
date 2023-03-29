ea56a6676175f2dd48de8e9bfede2f5acda9b2a5
Min_98 <40224148+pjm6401@users.noreply.github.com>
2023-03-30 00:00:19 +0900

    3/29 JWT Token

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -68,29 +68,8 @@ public class SecurityConfig implements PasswordEncoder{
         http.csrf().disable();
         http.httpBasic().disable();
         http.authorizeHttpRequests().requestMatchers("/*").permitAll();
-        //CORS 설정 메소드 체인
-        //http.cors().and();
-        /*http
-                .httpBasic().disable() // rest api 만을 고려하여 기본 설정은 해제
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 안함
-                .and()
-                /*
-                .authorizeRequests() → .authorizeHttpRequests()
-                  .antMatchers() → .requestMatchers()
-                 */
-                //.authorizeHttpRequests()
-                //.requestMatchers("/login").permitAll() // 토큰이없는경우 로그인기능만 이용 가능하도록 제한
-                //.anyRequest().authenticated();
-                //.and()
-                // CorsConfig 에서 Bean으로 등록한 corsFilter를 기존 Filter보다 앞서서 수행하겠다는 의미
-                //.addFilterBefore(corsFilter, SecurityContextHolderFilter.class)
-                //.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-                //.exceptionHandling()
-                /*
-                ExceptionHandlingConfigurer 객체에서 authenticationEntryPoint() 메서드를 호출하여,
-                인증되지 않은 사용자가 보호된 리소스에 접근하려고 시도할 때, 401 Unauthorized HTTP 응답 코드를 반환하도록 설정하는 것.
-                 */
-                //.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+      
+                
         return http.build();
     }
 }
